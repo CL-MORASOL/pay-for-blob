4a48394a18e598b61ae6d1ba0af73a374233318d
morazzela <morazzela@protonmail.com>
2023-05-07 08:21:17 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
 
   return (
     <div className="flex flex-wrap">
-      <div className="w-full lg:w-1/3 bg-white p-6">
+      <div className="w-full lg:w-1/3 bg-white p-6 border-r">
         <h1 className="text-2xl font-bold">Submit your {PFBName()} transaction</h1>
         <div className="bg-yellow-50 px-4 py-3 shadow-sm rounded text-yellow-800 border-l-4 border-yellow-600 mt-6">
           <p>When running a Celestia node, you will need to use the following flags when starting your node to submit a {PFBName()}:</p>
@@ -75,45 +75,50 @@ function App() {
         </a>
       </div>
       <div className="w-full lg:w-2/3 flex items-center justify-center lg:min-h-screen bg-gray-100 p-6">
-        <div className="bg-gray-100 rounded border bg-white w-full lg:w-1/2 p-6 my-12 lg:my-0">
+        <div className="bg-gray-100 shadow rounded bg-white w-full lg:w-1/2 p-6 my-12 lg:my-0">
           <form onSubmit={onSubmit}>
-            <div className="mb-2">
+            <div className="mb-4">
               <label className="block text-lg">Network</label>
-              <select onChange={({ target }: any) => { setNetwork(target.value) }} className="border rounded px-3 py-2">
+              <select disabled={sending} onChange={({ target }: any) => { setNetwork(target.value) }} className="border rounded px-3 py-2">
                 <option selected value="mocha">Mocha</option>
                 <option value="blockspacerace">Blockspace Race</option>
                 <option value="arabica">Arabica</option>
               </select>
             </div>
-            <div className="flex mb-2">
+            <div className="flex mb-4">
               <div className="w-2/3 pr-4">
                 <label className="block text-lg">Node IP Address</label>
-                <input type="text" className="border rounded px-3 py-2 w-full" defaultValue={domain} onInput={({ target }: any) => { setDomain(target.value.trim()) }}/>
-                  <small className="italic block mt-1 text-xs text-gray-500">Your node's public IP address.</small>
+                <input type="text" disabled={sending} className="border rounded px-3 py-2 w-full" defaultValue={domain} onInput={({ target }: any) => { setDomain(target.value.trim()) }}/>
+                <small className="italic block mt-1 text-xs text-gray-500">Your node's public IP address.</small>
               </div>
               <div className="w-1/3">
                 <label className="block text-lg">Port</label>
-                <input type="text" className="border rounded px-3 py-2 w-full" defaultValue={port} onInput={({ target }: any) => { setPort(target.value.trim()) }}/>
-                  <small className="italic block mt-1 text-xs text-gray-500">Must be open to public.</small>
+                <input type="text" disabled={sending} className="border rounded px-3 py-2 w-full" defaultValue={port} onInput={({ target }: any) => { setPort(target.value.trim()) }}/>
+                <small className="italic block mt-1 text-xs text-gray-500">Must be open to public.</small>
               </div>
             </div>
             <div className="flex">
               <div className="block pr-2 w-1/2">
                 <label className="block mb-1 text-lg">Namespace ID</label>
-                <input type="text" className="border rounded px-3 py-2 w-full" defaultValue={namespaceId} onInput={({ target }: any) => { setNamespaceId(target.value.trim()) }}/>
+                <input type="text" disabled={sending} className="border rounded px-3 py-2 w-full" defaultValue={namespaceId} onInput={({ target }: any) => { setNamespaceId(target.value.trim()) }}/>
               </div>
               <div className="block pl-2 w-1/2">
                 <label className="block mb-1 text-lg">Data</label>
-                <input type="text" className="border rounded px-3 py-2 w-full" defaultValue={data} onInput={({ target }: any) => { setData(target.value.trim()) }}/>
+                <input type="text" disabled={sending} className="border rounded px-3 py-2 w-full" defaultValue={data} onInput={({ target }: any) => { setData(target.value.trim()) }}/>
               </div>
             </div>
             <a href="https://go.dev/play/p/7ltvaj8lhRl" target="_blank" rel="noreferrer noopener" className="italic text-xs block mt-1 text-blue-400 hover:underline">Generate your namespace ID and data</a>
             {error && (
-              <div className="bg-red-100 p-4 my-6 rounded border border-red-200 text-red-900 font-bold flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                </svg>
-                <span className="ml-3">{error}</span>
+              <div className="bg-red-100 my-6 rounded border border-red-200 text-red-900 flex items-center">
+                <div className="w-16 flex justify-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                  </svg>
+                </div>
+                <div className="w-full py-4 pr-4">
+                  <div className="font-bold">{error}</div>
+                  <div>Make sure your node is properly configured</div>
+                </div>
               </div>
             )}
             {height !== 0 && txHash && (
